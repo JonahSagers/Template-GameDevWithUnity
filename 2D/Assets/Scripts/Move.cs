58b7f037b605d889beb768ac2328eb8d88c90639
JonahSagers <24sagjon@hawken.edu>
2024-03-05 13:34:08 -0500

    tuning and fastfall additions

--- 2D/Assets/Scripts/Move.cs
+++ 2D/Assets/Scripts/Move.cs
@@ -13,7 +13,11 @@ public class Move : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // rb.mass = 10;
+        // rb.drag = 10;
+        // rb.angularDrag = 10;
+        // rb.gravityScale = 10;
+        // rb.simulated = false;
     }
 
     // Update is called once per frame
@@ -27,13 +31,18 @@ public class Move : MonoBehaviour
         // }
         particles.gameObject.transform.position = transform.position;
         rb.velocity += Input.GetAxisRaw("Horizontal") * Vector2.right * Time.deltaTime * 20;
-        if(Input.GetKeyDown(KeyCode.Space) && Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y - 0.5f), 0.4f, ground)){
+        if(Input.GetKeyDown(KeyCode.Space) && Physics2D.OverlapCircle(transform.position + Vector3.down, 0.5f, ground)){
             rb.velocity = new Vector2(rb.velocity.x, 15);
             particles.Play();
         }
         if(Input.GetKeyDown(KeyCode.LeftShift) && dashCD == false){
             rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * 20, 2);
             StartCoroutine(DashCooldown(1));
+        }
+        if(Input.GetKey(KeyCode.LeftAlt)){
+            rb.gravityScale = 10;
+        } else {
+            rb.gravityScale = 2;
         }
     }
 
